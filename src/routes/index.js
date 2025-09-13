916ce3aa81a1109cfcf3448c3d0e3214fd7ca483
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-09-13 00:31:15 -0300

    referencias a rutas y controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const getAllSalones = require("../controllers/Salones/getAll");
 
+// Controllers Salones
+const getAllSalones = require("../controllers/Salones/getAll");
+const createSalon = require("../controllers/Salones/create");
+const updateSalon = require("../controllers/Salones/update");
+const deleteSalon = require("../controllers/Salones/delete");
+
+// Rutas CRUD Salones
 router.get('/salones', getAllSalones);
+router.post('/salones', createSalon);
+router.put('/salones/:id', updateSalon);
+router.delete('/salones/:id', deleteSalon);
 
 module.exports = router;
