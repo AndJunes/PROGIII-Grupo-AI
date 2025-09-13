88b45aea12b2081e7235af0516ee57c2da918578
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-09-13 02:26:51 -0300

    re-versionado

--- src/models/Salon.js
+++ src/models/Salon.js
@@ -2,47 +2,47 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../database/database');
 
 const Salon = sequelize.define('Salon', {
-  salon_id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  titulo: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  direccion: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  latitud: {
-    type: DataTypes.DECIMAL(10, 8),
-  },
-  longitud: {
-    type: DataTypes.DECIMAL(11, 8),
-  },
-  capacidad: {
-    type: DataTypes.INTEGER,
-  },
-  importe: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false,
-  },
-  activo: {
-    type: DataTypes.TINYINT,
-    defaultValue: 1,
-  },
-  creado: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  modificado: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
+    salon_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    titulo: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    direccion: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    latitud: {
+        type: DataTypes.DECIMAL(10, 8),
+    },
+    longitud: {
+        type: DataTypes.DECIMAL(11, 8),
+    },
+    capacidad: {
+        type: DataTypes.INTEGER,
+    },
+    importe: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
+    activo: {
+        type: DataTypes.TINYINT,
+        defaultValue: 1,
+    },
+    creado: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    modificado: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
 }, {
-  tableName: 'salones',
-  timestamps: false,
+    tableName: 'salones',
+    timestamps: false,
 });
 
 module.exports = Salon;
