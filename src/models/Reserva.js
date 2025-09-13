20c5b25fb4d5b0112201df99998c513c1e10294e
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-09-13 00:33:25 -0300

    referencia correcta a database

--- src/models/Reserva.js
+++ src/models/Reserva.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database');
+const sequelize = require('../database/database');
 
 const Reserva = sequelize.define('Reserva', {
   reserva_id: {
