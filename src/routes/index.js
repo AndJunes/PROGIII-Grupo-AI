9230323c8d977903b65cb8b94ed007e7035fd2b7
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-09-14 17:15:37 -0300

    agregamos rutas de reservas

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,18 @@ const { CLIENTE, EMPLEADO, ADMINISTRADOR } = require('../constants/roles');
 
 // Controllers Salones
 const SalonesController = require('../controllers/Salones/SalonesController');
+const ReservaController = require('../controllers/Salones/ReservaController')
 
 // Rutas de Salones
 router.get('/salones', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), SalonesController.getAll.bind(SalonesController));
 router.post('/salones', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.create.bind(SalonesController));
 router.put('/salones/:id', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.update.bind(SalonesController));
 router.delete('/salones/:id', auth, roleCheck([ADMINISTRADOR]), SalonesController.delete.bind(SalonesController));
+
+//Rutas de Reservas
+router.post('/reservas', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ReservaController.crear.bind(ReservaController));
+router.get('/reservas', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), ReservaController.listar.bind(ReservaController));
+
+
 
 module.exports = router;
