8a033bd1c39306b155a2d4ef1245881c620df9c0
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-09-26 20:42:24 -0300

    rutas de salones

---
+++ src/routes/salones.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const roleCheck = require('../middleware/roleCheck');
+const { CLIENTE, EMPLEADO, ADMINISTRADOR } = require('../constants/roles');
+const SalonesController = require('../controllers/Salones/SalonesController');
+
+// Salones
+router.get('/', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), SalonesController.getAll.bind(SalonesController));
+router.post('/', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.create.bind(SalonesController));
+router.put('/:id', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.update.bind(SalonesController));
+router.delete('/:id', auth, roleCheck([ADMINISTRADOR]), SalonesController.delete.bind(SalonesController));
+
+module.exports = router;
