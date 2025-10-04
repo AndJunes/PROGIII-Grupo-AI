b4c5737787bfb8dc9cc7e289429f659303f2cf78
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-10-03 23:09:45 -0300

    obtener salones especificos por id

--- src/routes/salones.js
+++ src/routes/salones.js
@@ -7,6 +7,7 @@ const SalonesController = require('../controllers/Salones/SalonesController');
 
 // Salones
 router.get('/', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), SalonesController.getAll.bind(SalonesController));
+router.get('/:id', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), SalonesController.getById.bind(SalonesController));
 router.post('/', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.create.bind(SalonesController));
 router.put('/:id', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), SalonesController.update.bind(SalonesController));
 router.delete('/:id', auth, roleCheck([ADMINISTRADOR]), SalonesController.delete.bind(SalonesController));
