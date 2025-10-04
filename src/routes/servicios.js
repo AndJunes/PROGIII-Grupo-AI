7216f176dc850b392cc7e955582df7b825f14d08
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-10-03 23:41:10 -0300

    rutas para consultas especificas

--- src/routes/servicios.js
+++ src/routes/servicios.js
@@ -6,8 +6,9 @@ const { CLIENTE, EMPLEADO, ADMINISTRADOR } = require('../constants/roles');
 const ServiciosController = require('../controllers/Servicios/ServiciosController');
 
 // Servicios
-router.get('/all', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ServiciosController.getAll.bind(ServiciosController));
 router.get('/', auth, roleCheck([CLIENTE, EMPLEADO, ADMINISTRADOR]), ServiciosController.getByUser.bind(ServiciosController));
+router.get('/all', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ServiciosController.getAll.bind(ServiciosController));
+router.get('/:id', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ServiciosController.getById.bind(ServiciosController));
 router.post('/', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ServiciosController.create.bind(ServiciosController));
 router.put('/:id', auth, roleCheck([EMPLEADO, ADMINISTRADOR]), ServiciosController.update.bind(ServiciosController));
 router.delete('/:id', auth, roleCheck([ADMINISTRADOR]), ServiciosController.delete.bind(ServiciosController));
