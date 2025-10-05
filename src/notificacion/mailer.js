7a5a73296356c2f51e1461e47fd33f280a9c9225
Facundo-Diaz <faku98_lomas@hotmail.es>
2025-10-05 18:22:56 -0300

    en vez de turno llamabamos a servicio, fix

--- src/notificacion/mailer.js
+++ src/notificacion/mailer.js
@@ -5,25 +5,24 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import Salon from '../models/Salon.js';
-import Servicio from '../models/Servicio.js';
+import Turno from '../models/Turno.js';
 
 // __dirname equivalente en ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export async function enviarNotificacion(reserva, usuario) {
+export async function enviarNotificacion(reserva, usuario, res = null) {
     try {
         const plantillaPath = path.join(__dirname, '../utils/plantilla.hbs');
         const archivoHbs = await readFile(plantillaPath, 'utf-8');
         const template = handlebars.compile(archivoHbs);
-
         const salon = await Salon.findByPk(reserva.salon_id);
-        const turno = await Servicio.findByPk(reserva.turno_id);
+        const turno = await Turno.findByPk(reserva.turno_id);
 
         const html = template({
             fecha: reserva.fecha_reserva,
             salon: salon ? salon.titulo : 'Sin nombre',
-            turno: turno ? turno.descripcion : 'Sin turno'
+            turno: turno ? `De ${turno.hora_desde}hs a ${turno.hora_hasta}hs` : 'Sin turno'
         });
 
         const transporter = nodemailer.createTransport({
@@ -64,5 +63,10 @@ export async function enviarNotificacion(reserva, usuario) {
         console.log('Notificaciones enviadas correctamente');
     } catch (err) {
         console.error('Error al enviar notificación:', err);
+
+        // Si se pasa res, responder con mensaje resumido
+        if (res) {
+            return res.status(500).json({ ok: false, mensaje: 'Error al enviar notificación' });
+        }
     }
 }
